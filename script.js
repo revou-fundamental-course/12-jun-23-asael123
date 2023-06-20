1c6a517b0832a925971df6d2116417769899b358
asael123 <ezraasaeltristan@gmail.com>
2023-06-21 00:34:55 +0700

    html css js

---
+++ script.js
@@ -0,0 +1,20 @@
+function operationcel () {
+    let cel = document.getElementById("celsius").value
+    console.log (cel);
+    let cel2 = cel * (9/5) + 32 ;
+    console.log (cel2);
+    let celinput = document.getElementById("farenheit");
+    celinput.value = cel2;
+    let cara = document.getElementById("cara")
+    cara.value = "untuk mendapatkan hasil Farenheit kita dapat mengkalikan angka tersebut dengan 9/5 lalu di tambahakan 32 seperti berikut:\n" + cel + " X 9/5 + 32 = " + cel2 ;
+
+
+}
+
+function opreationfar () {
+    let far = document.getElementById("farenheit").value
+    let far2 = (far - 32) * 5/9
+    let farinput = document.getElementById("celsius");
+    farinput.value = far2;
+
+}
