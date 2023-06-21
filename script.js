bc0000dd8a3fdac9b45216fabf2731abc4092e90
asael123 <ezraasaeltristan@gmail.com>
2023-06-21 18:51:41 +0700

    add design and reset function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function operationcel () {
     let cel = document.getElementById("celsius").value
     console.log (cel);
-    let cel2 = cel * (9/5) + 32 ;
+    let cel2 = ((cel * 9/5) + 32) .toFixed(1) ;
     console.log (cel2);
     let celinput = document.getElementById("farenheit");
     celinput.value = cel2;
@@ -13,8 +13,17 @@ function operationcel () {
 
 function opreationfar () {
     let far = document.getElementById("farenheit").value
-    let far2 = (far - 32) * 5/9
+    let far2 = ((far - 32) * 5/9).toFixed(1);
     let farinput = document.getElementById("celsius");
     farinput.value = far2;
+    let cara = document.getElementById("cara")
+    cara.value = "untuk mendapatkan hasil Celsius kita dapat mengkurangkan angka tersebut dengan 32 lalu di kalikan 5/9 seperti berikut:\n" + far + " - 32 X 5/9 = " + far2 ;
+}
+    
+
+function reset () {
+    document.getElementById("celsius").value = "";
+    document.getElementById("farenheit").value = "";
+    document.getElementById("cara").value = "";
 
 }
