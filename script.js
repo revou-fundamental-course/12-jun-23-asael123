4d0c4d0b6c043f57fa42fa4d79b9894af1c0c57b
asael123 <ezraasaeltristan@gmail.com>
2023-06-22 13:07:59 +0700

    html css js final temerature conversion

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 function operationcel () {
     let cel = document.getElementById("celsius").value
+    
     console.log (cel);
     let cel2 = ((cel * 9/5) + 32) .toFixed(1) ;
     console.log (cel2);
     let celinput = document.getElementById("farenheit");
     celinput.value = cel2;
+    if (cel === ""){
+        celinput.value = "";
+        }  
+        else 
+        celinput.value = cel2;
     let cara = document.getElementById("cara")
-    cara.value = "untuk mendapatkan hasil Farenheit kita dapat mengkalikan angka tersebut dengan 9/5 lalu di tambahakan 32 seperti berikut:\n" + cel + " X 9/5 + 32 = " + cel2 ;
+    if (cel === ""){
+        cara.value = "mohon untuk mengisi kotak yang telah disediakan";   
+        }  
+        else 
+        cara.value = "untuk mendapatkan hasil Farenheit kita dapat mengkalikan angka tersebut dengan 9/5 lalu di tambahakan 32 seperti berikut:\n" + cel + " X 9/5 + 32 = " + cel2 ;
 
+    }
 
-}
+    
 
 function opreationfar () {
     let far = document.getElementById("farenheit").value
     let far2 = ((far - 32) * 5/9).toFixed(1);
     let farinput = document.getElementById("celsius");
-    farinput.value = far2;
+    if (far === ""){
+        farinput.value = "";
+        }  
+        else 
+        farinput.value = far2;
     let cara = document.getElementById("cara")
-    cara.value = "untuk mendapatkan hasil Celsius kita dapat mengkurangkan angka tersebut dengan 32 lalu di kalikan 5/9 seperti berikut:\n" + far + " - 32 X 5/9 = " + far2 ;
+    if (far === ""){
+        cara.value = "mohon untuk mengisi kotak yang telah disediakan";   
+        }  
+        else 
+        cara.value = "untuk mendapatkan hasil Celsius kita dapat mengkurangkan angka tersebut dengan 32 lalu di kalikan 5/9 seperti berikut:\n" + far + " - 32 X 5/9 = " + far2 ;
 }
     
 
